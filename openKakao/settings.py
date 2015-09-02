9a507c71b8f66a95e34969d09465546e4203d93b
김혁준 <hjunkim@gimhyeogjun-ui-MacBook-Pro.local>
2015-09-02 18:57:51 +0900

    open kakao template

--- openKakao/settings.py
+++ openKakao/settings.py
@@ -107,11 +107,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(os.path.dirname(__file__), '..', 'index/templates').replace('\\','/'),
+    # os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR,'static_root')
 TEMPLATE_DIRS = (
     'index/templates',
 )
